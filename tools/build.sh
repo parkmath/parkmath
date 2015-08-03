a96e3f168ef0a73bb83a7cef8d55e308171ec005
Anand Thakker <vestibule@anandthakker.net>
2015-08-02 23:52:44 -0400

    Fix travis

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,11 @@
 set -e # halt script on error
 
 if [ $TRAVIS_PULL_REQUEST = "false" ]; then
-  jekyll build
+  bundle exec jekyll build
+  mkdir -p _site/books-prerendered
+  for filename in _site/books/*.html; do
+    ./node_modules/MathJax-node/bin/page2mml < "$filename" > "$build/books-prerendered/`basename $filename`";
+  done
 else
   echo "Not building, so long and thanks for all the fish!"
 fi
