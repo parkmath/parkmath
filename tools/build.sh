b4f4cf9e8f063b275d99c4f34d2d421031ce4671
Anand Thakker <vestibule@anandthakker.net>
2015-08-03 00:27:00 -0400

    Prerender in the build

--- tools/build.sh
+++ tools/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-if [ $TRAVIS_PULL_REQUEST = "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   bundle exec jekyll build
   mkdir -p _site/books-prerendered
   for filename in _site/books/*.html; do
-    ./node_modules/MathJax-node/bin/page2mml < "$filename" > "$build/books-prerendered/`basename $filename`";
+    echo "Prerendering equations for $filename"
+    ./tools/prerender.js --no-speech --extensions TeX/cancel < "$filename" > "_site/books-prerendered/`basename $filename`";
   done
 else
   echo "Not building, so long and thanks for all the fish!"
