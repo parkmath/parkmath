a7ef0477be0e46c47c413d2e3aab371f51eeb2bb
Anand Thakker <vestibule@anandthakker.net>
2016-08-03 09:54:28 -0400

    Fix typo

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@
 set -e
 
 make _site/pdf/01-reasoning-and-proving.pdf && ./tools/deploy.sh
-make _site/pdf/99-custom-2.pdf && ./tools/deploy.sh
+make _site/pdf/99-custom-1.pdf && ./tools/deploy.sh
 make _site/pdf/99-custom-2.pdf && ./tools/deploy.sh
 make all
 ./tools/deploy.sh
