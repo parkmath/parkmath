05fa60d23e9d45eec81901218fe7d0317f4e8b26
cukierm <cukierm@gmail.com>
2016-08-08 19:28:05 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -5,4 +5,4 @@ make _site/pdf/05-reasoning-and-proving-2.pdf && ./tools/deploy.sh
 #make _site/pdf/99-custom-1.pdf && ./tools/deploy.sh
 make _site/pdf/99-custom-2.pdf && ./tools/deploy.sh
 #make all
-./tools/deploy.sh
+#./tools/deploy.sh
