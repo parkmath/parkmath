edea1f9f98751a26a6e08a6fb6a1baca51c382cd
cukierm <cukierm@gmail.com>
2016-08-16 18:56:58 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-make _site/pdf/books/99-custom-1.pdf && ./tools/deploy.sh
-make _site/pdf/books/01-reasoning-and-proving.pdf && ./tools/deploy.sh
-make _site/pdf/books/99-custom-2.pdf && ./tools/deploy.sh
-make _site/pdf/books/05-reasoning-and-proving-2.pdf && ./tools/deploy.sh
-make _site/pdf/lessons/10-answers.pdf && ./tools/deploy.sh
+make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
 #make all
 #./tools/deploy.sh
