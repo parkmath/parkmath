76e26d3634dc0101e1e50f518a1d5d02da1e19c5
cukierm <cukierm@gmail.com>
2016-09-11 11:17:57 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
-#make all
-#./tools/deploy.sh
+#make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
+make all
+./tools/deploy.sh
