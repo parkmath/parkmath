1f22b866bc47234262a21b0636c4ab02798e20e8
cukierm <cukierm@gmail.com>
2016-10-06 14:52:03 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-#make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
-make all
+make _site/pdf/books/02-0-2-represent-symbolically.pdf && ./tools/deploy.sh
+#make all
 ./tools/deploy.sh
