693a154ce20afef5735fe06642d4488e2e37b7cc
cukierm <cukierm@gmail.com>
2016-10-06 16:41:56 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-make _site/pdf/books/02-0-2-represent-symbolically.pdf && ./tools/deploy.sh
+make _site/pdf/02-analytic-geometry.pdf && ./tools/deploy.sh
 #make all
 #./tools/deploy.sh
