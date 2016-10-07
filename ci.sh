649884bc4b4a7e7727d93856007c93a485417976
Anand Thakker <vestibule@anandthakker.net>
2016-10-07 05:37:11 -0400

    Fix CI script

--- ci.sh
+++ ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-make _site/pdf/02-analytic-geometry.pdf && ./tools/deploy.sh
+make _site/prerendered/books/02-analytic-geometry.html && ./tools/deploy.sh
+make _site/pdf/books/02-analytic-geometry.pdf && ./tools/deploy.sh
 #make all
 #./tools/deploy.sh
