17177b8adeb676809a15be72de2f5678bcd42caf
cukierm <cukierm@gmail.com>
2016-10-10 15:21:55 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-make _site/prerendered/books/02-analytic-geometry.html && ./tools/deploy.sh
-make _site/pdf/books/02-analytic-geometry.pdf && ./tools/deploy.sh
+make _site/prerendered/books/04-quadratics.html && ./tools/deploy.sh
+make _site/pdf/books/04-quadratics.pdf && ./tools/deploy.sh
 #make all
 #./tools/deploy.sh
