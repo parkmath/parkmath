fb081d530097bf14339df3efadd5401c6daaeb08
cukierm <cukierm@gmail.com>
2016-11-25 14:39:18 -0500

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-make _site/prerendered/books/Logarithms-and-Sundries.html && ./tools/deploy.sh
-make _site/pdf/books/Logarithms-and-Sundries.pdf && ./tools/deploy.sh
+make _site/prerendered/books/97-Logarithms-and-Sundries.html && ./tools/deploy.sh
+make _site/pdf/books/97-Logarithms-and-Sundries.pdf && ./tools/deploy.sh
 
 #make all
 #./tools/deploy.sh
