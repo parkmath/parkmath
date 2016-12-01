6841b2729a6f8f67f4765ddfc80bfc11cb35f2cb
cukierm <cukierm@gmail.com>
2016-12-01 13:31:44 -0500

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-make _site/prerendered/books/99-ways-of-counting-and-ways-of-modeling.html && ./tools/deploy.sh
-make _site/pdf/books/99-ways-of-counting-and-ways-of-modeling.html && ./tools/deploy.sh
+make _site/prerendered/books/97-ways-of-counting-and-ways-of-modeling.html && ./tools/deploy.sh
+make _site/pdf/books/97-ways-of-counting-and-ways-of-modeling.html && ./tools/deploy.sh
 
 #make all
 #./tools/deploy.sh
