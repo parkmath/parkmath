db4b9d0443cd169df3298654b8d90aaafb8f3d9e
cukierm <cukierm@gmail.com>
2017-07-17 12:10:52 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-make _site/prerendered/books/01-reasoning-and-proving.html.html && ./tools/deploy.sh
-make _site/pdf/books/01-reasoning-and-proving.pdf && ./tools/deploy.sh
+make _site/prerendered/books/05-reasoning-and-proving-2.html && ./tools/deploy.sh
+make _site/pdf/books/05-reasoning-and-proving-2.pdf && ./tools/deploy.sh
 
 #make _site/prerendered/books/06-geometry-and-proof.html && ./tools/deploy.sh
 #make _site/pdf/books/06-geometry-and-proof.pdf && ./tools/deploy.sh
