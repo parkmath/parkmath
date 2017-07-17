994d9c99307cab7d7e437be429a74238b4145c96
cukierm <cukierm@gmail.com>
2017-07-17 11:56:25 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-make _site/prerendered/books/03-investigating-shape-and-size.html && ./tools/deploy.sh
-make _site/pdf/books/03-investigating-shape-and-size.pdf && ./tools/deploy.sh
+make _site/prerendered/books/01-reasoning-and-proving.html.html && ./tools/deploy.sh
+make _site/pdf/books/01-reasoning-and-proving.pdf && ./tools/deploy.sh
 
 #make _site/prerendered/books/06-geometry-and-proof.html && ./tools/deploy.sh
 #make _site/pdf/books/06-geometry-and-proof.pdf && ./tools/deploy.sh
