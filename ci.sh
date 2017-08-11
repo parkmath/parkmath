a118f089d65372c6cb281a153dd511ef0db85880
cukierm <cukierm@gmail.com>
2017-08-10 20:29:52 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -7,6 +7,8 @@ set -e
 #make _site/pdf/books/99-custom-1.pdf && ./tools/deploy.sh
 make _site/prerendered/books/97-ways-of-counting-and-ways-of-modeling.html  && ./tools/deploy.sh
 make _site/pdf/books/97-ways-of-counting-and-ways-of-modeling.pdf  && ./tools/deploy.sh
+make _site/prerendered/books/05-reasoning-and-proving-2.html  && ./tools/deploy.sh
+make _site/pdf/books/05-reasoning-and-proving-2.html.pdf  && ./tools/deploy.sh
 
 #make _site/prerendered/books/06-geometry-and-proof.html && ./tools/deploy.sh
 #make _site/pdf/books/06-geometry-and-proof.pdf && ./tools/deploy.sh
