98e1eccfb4e86e55e36c55b647688dc2a351cf3f
cukierm <cukierm@gmail.com>
2017-08-11 11:43:52 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -3,10 +3,10 @@ set -e
 
 #make _site/prerendered/books/08-functions-and-models-1.html && ./tools/deploy.sh
 #make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
-#make _site/prerendered/books/99-custom-1.html && ./tools/deploy.sh
-#make _site/pdf/books/99-custom-1.pdf && ./tools/deploy.sh
-make _site/prerendered/books/97-ways-of-counting-and-ways-of-modeling.html  && ./tools/deploy.sh
-make _site/pdf/books/97-ways-of-counting-and-ways-of-modeling.pdf  && ./tools/deploy.sh
+make _site/prerendered/books/99-custom-1.html && ./tools/deploy.sh
+make _site/pdf/books/99-custom-1.pdf && ./tools/deploy.sh
+#make _site/prerendered/books/97-ways-of-counting-and-ways-of-modeling.html  && ./tools/deploy.sh
+#make _site/pdf/books/97-ways-of-counting-and-ways-of-modeling.pdf  && ./tools/deploy.sh
 #make _site/prerendered/books/05-reasoning-and-proving-2.html  && ./tools/deploy.sh
 #make _site/pdf/books/05-reasoning-and-proving-2.pdf  && ./tools/deploy.sh
 
