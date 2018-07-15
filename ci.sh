693aca8bc757d97506d8c2461895955bcb2a3412
cukierm <cukierm@gmail.com>
2018-07-15 12:03:58 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
-make _site/pdf/books/07-trigonometry-in-the-coordinate-plane.pdf && ./tools/deploy.sh
+
+make_site/pdf/books/01-reasoning-and-proving.pdf && ./tools/deploy.sh
+
+#make _site/pdf/books/07-trigonometry-in-the-coordinate-plane.pdf && ./tools/deploy.sh
 
 #make _site/pdf/books/03-investigating-shape-and-size.pdf && ./tools/deploy.sh
-#make _site/prerendered/books/08-functions-and-models-1.html && ./tools/deploy.sh
+
 #make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
-#make _site/prerendered/books/99-custom-1.html && ./tools/deploy.sh
+
 #make _site/pdf/books/99-custom-1.pdf && ./tools/deploy.sh
-#make _site/prerendered/books/97-ways-of-counting-and-ways-of-modeling.html  && ./tools/deploy.sh
+
 #make _site/pdf/books/97-ways-of-counting-and-ways-of-modeling.pdf  && ./tools/deploy.sh
-#make _site/prerendered/books/05-reasoning-and-proving-2.html  && ./tools/deploy.sh
+
 #make _site/pdf/books/05-reasoning-and-proving-2.pdf  && ./tools/deploy.sh
 
-#make _site/prerendered/books/06-geometry-and-proof.html && ./tools/deploy.sh
 #make _site/pdf/books/06-geometry-and-proof.pdf && ./tools/deploy.sh
 
 #make all
