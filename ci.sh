206907b774c8f65f47f7413ccbc6db5149df487c
cukierm <cukierm@gmail.com>
2018-07-19 10:43:57 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-#make _site/pdf/books/04-quadratics.pdf && ./tools/deploy.sh
+make _site/pdf/books/04-quadratics.pdf && ./tools/deploy.sh
 
-#make _site/pdf/books/02-analytic-geometry.pdf && ./tools/deploy.sh
+make _site/pdf/books/02-analytic-geometry.pdf && ./tools/deploy.sh
 
 #make _site/pdf/books/01-reasoning-and-proving.pdf && ./tools/deploy.sh
 
