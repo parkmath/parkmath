a7bacc38998be3cce0400517535b831278d9633d
cukierm <cukierm@gmail.com>
2018-08-09 11:32:04 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -5,11 +5,11 @@ set -e
 
 make _site/pdf/books/02-analytic-geometry.pdf && ./tools/deploy.sh
 
-make _site/pdf/books/01-reasoning-and-proving.pdf && ./tools/deploy.sh
+#make _site/pdf/books/01-reasoning-and-proving.pdf && ./tools/deploy.sh
 
 #make _site/pdf/books/07-trigonometry-in-the-coordinate-plane.pdf && ./tools/deploy.sh
 
-make _site/pdf/books/03-investigating-shape-and-size.pdf && ./tools/deploy.sh
+#make _site/pdf/books/03-investigating-shape-and-size.pdf && ./tools/deploy.sh
 
 #make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
 
