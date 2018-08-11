aaded87d6e72f5cf0cbc95dda9cb992040459f22
cukierm <cukierm@gmail.com>
2018-08-11 13:08:57 -0400

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -11,11 +11,11 @@ set -e
 
 #make _site/pdf/books/03-investigating-shape-and-size.pdf && ./tools/deploy.sh
 
-#make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
+make _site/pdf/books/08-functions-and-models-1.pdf && ./tools/deploy.sh
 
 #make _site/pdf/books/99-custom-1.pdf && ./tools/deploy.sh
 
-make _site/pdf/books/97-ways-of-counting-and-ways-of-modeling.pdf  && ./tools/deploy.sh
+#make _site/pdf/books/97-ways-of-counting-and-ways-of-modeling.pdf  && ./tools/deploy.sh
 
 #make _site/pdf/books/05-reasoning-and-proving-2.pdf  && ./tools/deploy.sh
 
